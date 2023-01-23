680f75cb3f9e56848b1a996ecb002113d932e677
nishant <nyindorkar@gmail.com>
2023-01-23 19:09:04 +0530

    email

--- tomcat.groovy
+++ tomcat.groovy
@@ -49,6 +49,7 @@ pipeline {
         post {
         always {
             emailext body: 'successfully tomcat server started', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'tomcat server started started'
+            }
         }
     }
 }
