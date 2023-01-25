aa41f7ed616d454c52f18d07c69b8cab409a58c4
nishant <nyindorkar@gmail.com>
2023-01-25 11:59:11 +0530

    email change

--- tomcat.groovy
+++ tomcat.groovy
@@ -59,7 +59,7 @@ pipeline {
                     recipientProviders: [[$class: 'CulpritsRecipientProvider']]
                 }
             }
-}  
+        }  
     }
     // post {
     //     always {
