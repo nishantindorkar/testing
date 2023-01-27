b8cad11a1c7532d87488032144ed3d4fa316d0e7
nishant <nyindorkar@gmail.com>
2023-01-27 12:10:18 +0530

    onlinebook

--- slackNotifier.groovy
+++ slackNotifier.groovy
@@ -1,16 +1,28 @@
-#!/usr/bin/env groovy
-
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear"	
-  }
+pipeline {
+    agent any
+    stages {
+        stage("git-pull") {
+            steps { 
+                sh 'sudo apt-get update -y'
+                //sh 'sudo apt-get install git -y'
+                git credentialsId: 'one', url: 'git@github.com:nishantindorkar/onlinebookstore.git'
+            }
+        }
+        stage("build-maven") {
+            steps { 
+                //sh 'sudo apt-get update -y'
+                //sh 'sudo apt-get install maven curl unzip -y'
+                sh 'mvn clean package'
+            }
+        }
+        stage("build-artifacts") {
+            steps { 
+                // sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
+                // sh 'unzip awscliv2.zip'
+                // sh 'sudo ./aws/install'
+                sh 'aws s3 mb s3://studentngpbckt'
+                sh 'aws s3 cp **/*.war s3://studentngpbckt/student-${BUILD_ID}.war'
+            }
+        }
+    }
 }
